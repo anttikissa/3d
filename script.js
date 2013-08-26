cfc4ca2df224bfd59fe1935abd8737ab8e353ada
Antti Sykäri <antti.sykari@gmail.com>
2013-08-26 15:49:30 +0300

    Basic world.

--- script.js
+++ script.js
@@ -8,21 +8,47 @@ renderer.setSize(width, height);
 
 scene.add(camera);
 
-var geometry = new THREE.CubeGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-var cube = new THREE.Mesh(geometry, material);
+function plane() {
+	var n = 20;
+	var geometry = new THREE.PlaneGeometry(n, n, n, n);
+	var material = new THREE.MeshBasicMaterial(
+		{ color: 0x00ff00, wireframe: true });
+	var plane = new THREE.Mesh(geometry, material);
+	plane.rotation.x += Math.PI / 2;
 
-scene.add(cube);
+	scene.add(plane);
+}
 
-camera.position.z = 5;
+function ship() {
+	var geometry = new THREE.CubeGeometry(1, 1, 1);
+	var material = new THREE.MeshBasicMaterial(
+		{ color: 0xff0000, wireframe: true });
+	var ship = new THREE.Mesh(geometry, material);
+
+	ship.position.y = 0.5;
+	scene.add(ship);
+}
+
+plane();
+ship();
+
+camera.position.z = 15;
+camera.position.y = 2;
 
 document.body.appendChild(renderer.domElement);
 
 function render() {
 	requestAnimationFrame(render);
-//	cube.rotation.x += 0.04;
-//	cube.rotation.y += 0.04;
+//	plane.rotation.y += 0.01;
 	renderer.render(scene, camera);
 }
 
 render();
+
+window.onkeyup = function(e) {
+	console.log(e.keyCode);
+}
+
+window.onkeydown = function(e) {
+	console.log(e.keyCode);
+}
