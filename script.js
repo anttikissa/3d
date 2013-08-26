f5da5f14aa39045cee97200f20acbb19be211456
Antti Sykäri <antti.sykari@gmail.com>
2013-08-26 16:03:59 +0300

    components stuff and simple camera.

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
+//// Basic three.js init
+
 var width = window.innerWidth - 10;
 var height = window.innerHeight - 10;
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
+var camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
 var renderer = new THREE.WebGLRenderer(); 
 renderer.setSize(width, height);
 
 scene.add(camera);
 
-function plane() {
+camera.position.z = 15;
+camera.position.y = 2;
+
+document.body.appendChild(renderer.domElement);
+
+
+
+//// Keyboard handling
+
+var keyname = require('keyname');
+
+// Keys that are down.
+var keys = {};
+
+window.onkeyup = function(e) {
+	var key = keyname(e.keyCode);
+	if (key) {
+		keys[key] = false;
+	}
+}
+
+window.onkeydown = function(e) {
+	var key = keyname(e.keyCode);
+	if (key) {
+		keys[key] = true;
+	}
+}
+
+
+
+//// Objects
+
+function Plane() {
 	var n = 20;
 	var geometry = new THREE.PlaneGeometry(n, n, n, n);
 	var material = new THREE.MeshBasicMaterial(
@@ -17,9 +51,11 @@ function plane() {
 	plane.rotation.x += Math.PI / 2;
 
 	scene.add(plane);
+
+	return plane;
 }
 
-function ship() {
+function Ship() {
 	var geometry = new THREE.CubeGeometry(1, 1, 1);
 	var material = new THREE.MeshBasicMaterial(
 		{ color: 0xff0000, wireframe: true });
@@ -27,28 +63,40 @@ function ship() {
 
 	ship.position.y = 0.5;
 	scene.add(ship);
+
+	return ship;
 }
 
-plane();
-ship();
+var plane = Plane();
+var ship = Ship();
 
-camera.position.z = 15;
-camera.position.y = 2;
+//// Camera
 
-document.body.appendChild(renderer.domElement);
+var turnSpeed = 0.02;
 
-function render() {
-	requestAnimationFrame(render);
-//	plane.rotation.y += 0.01;
+function frame() {
+	requestAnimationFrame(frame);
+
+	// FPS-style camera
+	if (keys.left) {
+		camera.rotation.y += turnSpeed;
+	}
+
+	if (keys.right) {
+		camera.rotation.y -= turnSpeed;
+	}
+
+	if (keys.up) {
+		camera.rotation.x += turnSpeed;
+	}
+
+	if (keys.down) {
+		camera.rotation.x -= turnSpeed;
+	}
+
+	// render
 	renderer.render(scene, camera);
 }
 
-render();
+frame();
 
-window.onkeyup = function(e) {
-	console.log(e.keyCode);
-}
-
-window.onkeydown = function(e) {
-	console.log(e.keyCode);
-}
